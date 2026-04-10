4bcb981c736a9dadbdab927a1fd7a1a5b4daadb2
Christian Goeschel Ndjomouo <cgoesc2@wgu.edu>
2026-04-09 23:40:29 -0400

    tests: (run.sh) fix coding style
    Signed-off-by: Christian Goeschel Ndjomouo <cgoesc2@wgu.edu>

--- tests/run.sh
+++ tests/run.sh
@@ -193,7 +193,7 @@ if [ -n "$SUBTESTS" ]; then
 		fi
 	done
 else
-	if [ -z "$SYSCOMMANDS" -a ! -f "$top_builddir/test_ttyutils" ]; then
+	if [ -z "$SYSCOMMANDS" ] && [ ! -f "$top_builddir/test_ttyutils" ]; then
 		echo "Tests not compiled! Run 'make check-programs' to fix the problem."
 		exit 1
 	fi
